ee147d661c5dbd1247b2a61eb6e2e3121966df30
Alexander Willemsen <alexanderwillemsen@msn.com>
2016-03-22 11:00:00 +0100

    Forms cleanup
    + improved error reporting
    + improved layout

--- realestate/forms.py
+++ realestate/forms.py
@@ -4,3 +4,7 @@ class FeedbackForm(forms.Form):
     from_email = forms.EmailField(label="E-mail")
     subject = forms.CharField(label="Onderwerp")
     message = forms.CharField(label="Vraag",widget=forms.Textarea)
+    def __init__(self, *args, **kwargs):
+        super(FeedbackForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
