9b55579c12fa110657a95db9368399dad38d3b50
BartDeCaluwe <de.caluwe.bart@gmail.com>
2016-04-19 14:26:07 +0200

    Switched the default DB from sqlite to Mysql
    Added the settings to target a MySql db instead of a Sqlite db.

--- viasofie/settings.py
+++ viasofie/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for viasofie project.
 
@@ -84,9 +85,19 @@ WSGI_APPLICATION = 'viasofie.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'viasofie',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'OPTIONS': {
+          'autocommit': True,
+          'read_default_file': '/mysqlConfig.cnf',
+        },
     }
 }
 
