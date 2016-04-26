f8b4a328433e5ab5522ad6c4e1ea9e64c2e81635
Alexander Willemsen <alexanderwillemsen@msn.com>
2016-04-26 22:16:00 +0200

    changed Admin panel Model icons
    +bootstrap css & js
    +images loading due to other directory

--- viasofie/settings.py
+++ viasofie/settings.py
@@ -150,7 +150,7 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
