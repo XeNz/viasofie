ea1d1b42f874e9b73bf967848d375acf4d9714a1
s085869 <michelle.vannuffel@student.ap.be>
2016-05-30 12:52:22 +0200

    Added translation tags

--- viasofie/settings.py
+++ viasofie/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
       #SQLITE
       # 'default': {
       #     'ENGINE': 'django.db.backends.sqlite3',
-      #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-      # },
-    #
+       #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # },
+    #   
     # SCHOOL VPS
     # 'default': {
     #  'ENGINE': 'django.db.backends.mysql',
