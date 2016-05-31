806a6d013ee4aa5f114707d442c98f145fccb367
Alexander Willemsen <xentricator@gmail.com>
2016-05-31 11:29:15 +0200

    Username added to UserChangeForm
    still need to find out method on how to mail reset pw

--- realestate/admin.py
+++ realestate/admin.py
@@ -8,22 +8,26 @@ from reportlab.graphics.barcode.qr import QrCodeWidget
 from reportlab.graphics import renderPDF
 from datetime import datetime
 from django import forms
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from realestate.models import ClientUser
-
+from django.contrib.auth import (
+    authenticate, get_user_model, password_validation,
+)
 
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
+    username = forms.CharField(label='Username')
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=False)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput, required=False)
 
+    widgets = {'username': forms.CharField(required=True)}
     class Meta:
         model = ClientUser
-        fields = ('email',)
-
+        fields = ('email','username',)
+        # field_classes = {'username': UsernameField}
     def clean_password2(self):
         # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
@@ -35,11 +39,11 @@ class UserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
+        user.set_password(self.cleaned_data["password1"])   
+        username = self.cleaned_data.get("username")
+        if commit:      
             user.save()
         return user
-
 
 class UserChangeForm(forms.ModelForm):
     """A form for updating users. Includes all the fields on
@@ -50,8 +54,8 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = ClientUser
-        fields = ('email', 'password', 'is_active', 'is_admin')
-
+        fields = ('email', 'password', 'is_active', 'is_admin', 'username')
+        # field_classes = {'username': UsernameField}
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
         # This is done here, rather than on the field, because the
@@ -67,10 +71,10 @@ class ClientUserAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'is_admin')
+    list_display = ('email', 'username','is_admin')
     list_filter = ('is_admin',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', 'username')}),
         ('Personal info', {'fields': ()}),
         ('Permissions', {'fields': ('is_admin',)}),
     )
@@ -79,7 +83,7 @@ class ClientUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
+            'fields': ('email', 'username', 'password1', 'password2')}
         ),
     )
     search_fields = ('email',)
